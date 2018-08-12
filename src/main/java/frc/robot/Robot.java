faf7265a7d524c80a6ee9b36e35d60416ae2bf00
Paul <paul.a.terrasi@gmail.com>
2018-08-12 13:35:58 -0400

    Initial Commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation.
+ */
+public class Robot extends IterativeRobot {
+
+  /**
+   * Runs when the robot is first powered on.
+   */
+  @Override
+  public void robotInit() {
+  }
+
+  /**
+   * Runs whenever a driver station packet is received, directly following
+   * the mode specific periodic functions (disabledPeriodic(), 
+   * teleopPeriodic(), etc.).
+   */
+  @Override
+  public void robotPeriodic() {
+  }
+
+  /**
+   * Runs whenever the robot receives a packet during teleop mode.
+   */
+  @Override
+  public void teleopPeriodic() {
+  }
+}
